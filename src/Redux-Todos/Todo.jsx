4d066fd746fbf5578bd204ac17662d1d0e3e1055
ranasikder1122 <ranasikder90755@gmail.com>
2025-07-09 14:30:57 +0600

    css add

--- src/Redux-Todos/Todo.jsx
+++ src/Redux-Todos/Todo.jsx
@@ -43,11 +43,11 @@ const handleEditTodo =(todo)=>{
   return (
     <div  className='min-h-screen bg-gray-100 flex flex-col items-center py-10 '>
         <h1 className='text -4xl font-bold mb-5'>Redux Todo App</h1>
-        <div className='w-11/12 max-w-4xl space-y-7 '>
+        <div className='w-11/12 max-w-4xl space-y-4 '>
             <div className='flex items-center space-x-3 '>
                   <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder='Enter Your Todo List'
-                  className='flex-grow px-4 py-4 border rounded-md focus:outline-none'/>
-                  <button className='px-p py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600' 
+                  className='flex-grow px-4 py-2 border rounded-md focus:outline-none'/>
+                  <button className='px-2 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600' 
                   onClick={handleAddTodos}>{edit ?'Edit Todo':'Add Todo'}</button>
             </div>
             <div>
